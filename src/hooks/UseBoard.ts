2558a80dab2dc7e1d6570fd346d014dae633faec
serogbp <49165676+serogbp@users.noreply.github.com>
2022-08-15 16:28:57 +0200

    Separar lógica de Board

---
+++ src/hooks/UseBoard.ts
@@ -0,0 +1,200 @@
+import { useEffect, useState } from "react"
+import { DragStart, DropResult, ResponderProvided } from "react-beautiful-dnd"
+import { UseWebs } from "./UseWebs"
+import { jsonContent, Web } from "../Types"
+
+export default function UseBoard() {
+	const [state, setState] = useState<jsonContent>(UseWebs().getWebs())
+
+
+	useEffect(() => {
+		// TODO actualizar localstorage
+	}, [state])
+
+
+	const handleAddWeb = (web: Web, category: string) => {
+		web.id = Object.keys(state.webs).length
+
+		const newWebs = {
+			...state.webs,
+			[web.id]: web
+		}
+		const newWebIds = [...state.categories[category].webIds, web.id]
+		const newCategories = {
+			...state.categories,
+			[category]: {
+				...state.categories[category],
+				webIds: newWebIds
+			}
+		}
+		const newState: jsonContent = {
+			...state,
+			webs: newWebs,
+			categories: newCategories
+		}
+		setState(newState)
+	}
+
+
+	const handleUpdateWeb = (updatedWeb: Web, destinationCategory: string, originCategory: string) => {
+		let newCategories = {
+			...state.categories
+		}
+
+		// Mover de columna
+		if (destinationCategory !== originCategory) {
+			const oldColumn = state.categories[originCategory]
+			oldColumn.webIds = oldColumn.webIds.filter(item => item !== updatedWeb.id)
+
+			const newColumn = state.categories[destinationCategory]
+			newColumn.webIds = [...newColumn.webIds, updatedWeb.id]
+
+			newCategories = {
+				...state.categories,
+				[originCategory]: oldColumn,
+				[destinationCategory]: newColumn,
+			}
+		}
+
+		const newState = {
+			...state,
+			categories: newCategories,
+			webs: {
+				...state.webs,
+				[updatedWeb.id]: updatedWeb
+			}
+		}
+		setState(newState)
+	}
+
+
+	const handleDeleteWeb = (web: Web, category: string) => {
+		const newWebs = state.webs
+		delete newWebs[web.id]
+
+		const newCategories = {
+			...state.categories,
+			[category]: {
+				...state.categories[category],
+				webIds: state.categories[category].webIds.filter(id => id !== web.id)
+			}
+		}
+
+		const newState: jsonContent = {
+			...state,
+			webs: newWebs,
+			categories: newCategories,
+			categoryOrder: state.categoryOrder
+		}
+
+		setState(newState)
+	}
+
+
+	const handlerDragStart = (initial: DragStart, provided: ResponderProvided) => {
+		console.log("DRAG START")
+		console.log(initial)
+		console.log(provided)
+	}
+
+
+	const handlerDragEnd = (result: DropResult) => {
+		switch (result.type) {
+			case "columnItem":
+				columnItemDragEnd(result)
+				break
+			case "column":
+				columnDragEnd(result)
+				break
+			default:
+				break
+		}
+	}
+
+
+	const columnItemDragEnd = (result: DropResult) => {
+		const { source, destination, draggableId } = result
+		// Si hace drop en una zona no permitida
+		if (!destination) {
+			return
+		}
+		else {
+			// Si hace drop en la posición original
+			if (destination.droppableId === source.droppableId && destination.index === source.index)
+				return
+
+			// La web ha sido droppeada en la misma categoría
+			// Solo cambiar index de la web
+			if (destination.droppableId === source.droppableId) {
+				const newColumn = state.categories[source.droppableId]
+				newColumn.webIds.splice(source.index, 1) // Eliminar antiguo index
+				newColumn.webIds.splice(destination.index, 0, parseInt(draggableId)) // Mover web modificada a nuevo index
+
+				const newState = {
+					...state,
+					categories: {
+						...state.categories,
+						[source.droppableId]: newColumn
+					}
+				}
+				setState(newState)
+			}
+
+			// La web ha sido droppeada en otra categoría
+			// Mover web a otra categoría
+			if (destination.droppableId !== source.droppableId) {
+				const oldColumn = state.categories[source.droppableId]
+				oldColumn.webIds.splice(source.index, 1)
+
+				const newColumn = state.categories[destination.droppableId]
+				newColumn.webIds.splice(destination.index, 0, parseInt(draggableId))
+
+				const newState = {
+					...state,
+					categories: {
+						...state.categories,
+						[source.droppableId]: oldColumn,
+						[destination.droppableId]: newColumn
+					}
+				}
+				setState(newState)
+			}
+		}
+	}
+
+
+	const columnDragEnd = (result: DropResult) => {
+		const { source, destination, draggableId } = result
+		// Si hace drop en una zona no permitida
+		if (!destination) {
+			return
+		}
+		else {
+			// Si hace drop en la posición original
+			if (destination.index === source.index)
+				return
+
+			const newCategoryOrder = state.categoryOrder
+			newCategoryOrder.splice(source.index, 1)
+			newCategoryOrder.splice(destination.index, 0, draggableId)
+
+			const newState = {
+				...state,
+				columnOrder: newCategoryOrder
+			}
+			setState(newState)
+		}
+	}
+
+
+	return {
+		state,
+		columnDragEnd,
+		columnItemDragEnd,
+		handleAddWeb,
+		handleDeleteWeb,
+		handleUpdateWeb,
+		handlerDragEnd,
+		handlerDragStart
+	}
+}
