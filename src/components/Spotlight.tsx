9d9d4d05243cd71a08ce79f145810809a23a3e20
serogbp <49165676+serogbp@users.noreply.github.com>
2022-09-25 15:38:35 +0200

    Spotlight fuzzy search

--- src/components/Spotlight.tsx
+++ src/components/Spotlight.tsx
@@ -1,15 +1,16 @@
 import { Search, Plus, Settings, Link } from "tabler-icons-react";
-import { SpotlightActionProps, SpotlightProvider } from '@mantine/spotlight';
-import { UnstyledButton, Group, Center, createStyles, Text } from '@mantine/core';
+import { SpotlightProvider } from '@mantine/spotlight';
 import type { SpotlightAction } from '@mantine/spotlight';
-import { useBoard } from "../hooks/useBoard/UseBoard";
+import {useBoard } from "../hooks/useBoard/UseBoard";
 import { useModal } from "../hooks/UseModal";
 import { CustomAction } from "./CustonAction";
+import fuzzysort from "fuzzysort";
 
 
 export function Spotlight(props: any) {
 	const board = useBoard()
 	const modal = useModal
+	const LIMIT_OF_ACTIONS_ON_SCREEN = 8
 
 	const orderedWebs = Object.values(board.state.webs).sort((a, b) => b.stats.lastClickTimestamp - a.stats.lastClickTimestamp)
 
@@ -17,11 +18,11 @@ export function Spotlight(props: any) {
 		return {
 			title: web.name,
 			description: `${web.url} ${web.tags}`,
-			group: "Webs",
+			group: "Recent opened webs",
 			onTrigger: () => openWeb(web.url),
 			icon: <Link size={18} />,
 			web: web,
-			category: board.category.getName(web)
+			category: board.category.getName(web),
 		}
 	})
 
@@ -32,26 +33,26 @@ export function Spotlight(props: any) {
 				title: "New web",
 				group: "Actions",
 				onTrigger: modal.webAdd,
-				icon: <Plus size={18} />
+				icon: <Plus size={18} />,
 			},
 			{
 				id: "addColumn",
 				title: "New category",
 				group: "Actions",
 				onTrigger: modal.category,
-				icon: <Plus size={18} />
+				icon: <Plus size={18} />,
 			},
 			{
 				id: "settings",
 				title: "Settings",
 				group: "Actions",
 				onTrigger: modal.settings,
-				icon: <Settings size={18} />
+				icon: <Settings size={18} />,
 			},
 		]
 
-	// Colocar al final de la lista de acciones las fixed
-	webActions.splice(7, 0, ...fixedActions)
+	// Colocar al final de la lista las fixedActions
+	webActions.splice(LIMIT_OF_ACTIONS_ON_SCREEN - fixedActions.length, 0, ...fixedActions)
 
 
 	const openWeb = (url: string) => {
@@ -61,6 +62,62 @@ export function Spotlight(props: any) {
 		link.rel = "noopener noreferrer nofollow";
 		link.click()
 	}
+
+	// Filtrar usando fuzzy search y ordenar por el fuzzy search score
+	// Si es web, filtra por nombre, url, categoría y tags
+	// Si es una acción de la app, filtra por title y description
+	// TODO highlight de mantine no funciona con fuzzy search, solo con búsqueda normal
+	function customBasicFilter(query: string, actions: SpotlightAction[]) {
+		if (query === "") return actions
+		return fuzzysort.go(query, actions, {limit: LIMIT_OF_ACTIONS_ON_SCREEN, keys: ["title", "description", "category", "web.tags"]}).map(result => result.obj)
+	}
+
+
+	// Lo mismo que customBasicFilter añadiendo filtrado por categorías y tags usando un carácter especial para cada uno (@ y # respectivamente)
+	// TODO Por ahora WIP
+	// function customAdvanceFilter(query: string, actions: SpotlightAction[]) {
+	// 	if (query === "") return actions
+
+	// 	const categories: string[] = []
+	// 	const tags: string[] = []
+	// 	const alphas: string[] = []
+
+	// 	query.split(" ").forEach(q => {
+	// 		if (q.length === 1){
+	// 			alphas.push(q)
+	// 			return
+	// 		}
+
+	// 		const first = q[0]
+	// 		const word = q.substring(1).toLowerCase()
+
+	// 		if (first === "@") categories.push(word)
+	// 		else if (first === "#") tags.push(word)
+	// 		else alphas.push(word)
+	// 	})
+
+	// 	let filteredActions = actions
+
+	// 	if (categories.length > 0) {
+	// 		// filteredActions = filteredActions.filter(action => {
+	// 		// 	return action.category ? categories.includes(action.category.toLowerCase()) : false
+	// 		// })
+	// 		const result = fuzzysort.go(categories.join(" "), filteredActions, {limit: 10, keys: ["category"]})
+	// 		filteredActions = result.map(r => r.obj)
+	// 	}
+
+	// 	if (tags.length > 0) {
+	// 		const result = fuzzysort.go(categories.join(" "), filteredActions, {limit: 10, keys: ["web.tags"]})
+	// 		filteredActions = result.map(r => r.obj)
+	// 	}
+
+	// 	if (alphas.length > 0) {
+	// 		const result = fuzzysort.go(alphas.join(" "), filteredActions, {limit: 10, keys: ["title", "description"]})
+	// 		filteredActions = result.map(r => r.obj)
+	// 	}
+
+	// 	return filteredActions
+	// }
 
 
 	return (
@@ -72,6 +129,9 @@ export function Spotlight(props: any) {
 			shortcut={['mod + P', 'mod + K', '/']}
 			nothingFoundMessage="Nothing found..."
 			highlightQuery
+			centered
+			limit = {LIMIT_OF_ACTIONS_ON_SCREEN}
+			filter={(query, actions) => customBasicFilter(query, actions)}
 		>
 			{props.children}
 		</SpotlightProvider>
